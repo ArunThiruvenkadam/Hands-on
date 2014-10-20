654ae2e0bd078bd6d7dab47cf3cf92857b30770e
ArunThiruvenkadam <arun.thiruvenkadam@object-frontier.com>
2014-10-20 21:05:44 +0530

    js file updated

--- js/index.js
+++ js/index.js
@@ -5,16 +5,7 @@ function validateLogin() {
 	var uName = document.forms["myForm"]["username"].value;
 	var pwd = document.forms["myForm"]["password"].value;
 	
-    if ((uName == null || uName == "") && (pwd == null || pwd == "")) {
-        alert("Enter the Username & Password.");
-        return false;
-    } else if (uName==null || uName=="") {
-	   alert("Enter the Username.");
-        return false;
-	} else if (pwd==null || pwd=="") {
-	   alert("Enter the Password.");
-        return false;
-	} else if("arun@ofs" != uName || "pass" != pwd) {
+    if("arun@ofs" != uName || "pass" != pwd) {
 		alert("Invalid Username/Password.");
         return false;
 	}
