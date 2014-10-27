54f4768440e460e2ecb9a198c236ec2c0723e225
ArunThiruvenkadam <arun.thiruvenkadam@object-frontier.com>
2014-10-27 10:39:04 +0530

    Login credential validation moved to JS

--- js/index.js
+++ js/index.js
@@ -5,6 +5,22 @@ function validateLogin() {
 	var uName = document.forms["myForm"]["username"].value;
 	var pwd = document.forms["myForm"]["password"].value;
 	
+	if((uName == null || uName == "") && (pwd == null || pwd == "")) {
+		alert("Enter username & password");
+		return false;
+	}
+	if(uName == null || uName == "") {
+		alert("Enter username");
+		return false;
+	}
+	if(pwd == null || pwd == "") {
+		alert("Enter password");
+		return false;
+	}
+	if(pwd.length < 6) {
+		alert("Password should be atleast 6 characters minimum");
+		return false;
+	}
     if("arun@ofs" != uName || "password" != pwd) {
 		alert("Invalid Username/Password.");
         return false;
